09ad52768c50916648d7d8efa7e32488ea0e60d6
Duratorre <t.duratorre@gmail.com>
2021-07-28 02:27:23 +0200

    Modified run.py

--- app/run.py
+++ app/run.py
@@ -46,7 +46,7 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('etl_data', engine)
 
 # load model
-model = joblib.load("../models/Classifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
